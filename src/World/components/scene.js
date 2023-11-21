25d6f955e33ecb2e0f38256cf31263ba72a7a4b3
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-21 22:36:32 +1300

    change scene color for testing

--- src/World/components/scene.js
+++ src/World/components/scene.js
@@ -2,7 +2,7 @@ import { Color, Scene } from '../../../node_modules/three/build/three.module.js'
 
 function createScene(){
     const scene = new Scene();
-    scene.background = new Color('skyblue');
+    scene.background = new Color('red');
     return scene;
 }
 export{createScene};
