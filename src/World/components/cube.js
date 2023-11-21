7ba48310f9bdadbbf579900d008f452f70b32560
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-21 23:06:48 +1300

    update

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -5,7 +5,7 @@ function createCube(first){
     const material = new MeshBasicMaterial();
     const cube = new Mesh(geometry, material);
     const cube2 = new Mesh(geometry, material);
-    cube2.position.set(2.5, 1.7)
+    cube2.position.set(2.5, 1.7);
     if(first == true){
         return (cube);
     }
