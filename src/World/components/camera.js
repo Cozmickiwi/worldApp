10880c9948ea73b50fea4feabcfc916acbe4757d
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-21 22:36:18 +1300

    fix typo

--- src/World/components/camera.js
+++ src/World/components/camera.js
@@ -2,7 +2,7 @@ import { PerspectiveCamera } from '../../../node_modules/three/build/three.modul
 
 function createCamera(){
     const camera = new PerspectiveCamera(35, 1, 0.1, 100,);
-    camera.getWorldPosition.set(0, 0, 10);
+    camera.position.set(0, 0, 10);
     return camera;
 }
 export{createCamera};
