35886efa19b52194b0ee846a4941345e8fefbe07
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-21 22:37:43 +1300

    add modules to world class

--- src/World/World.js
+++ src/World/World.js
@@ -6,8 +6,18 @@ import{createScene} from './components/scene.js';
 import{createRenderer} from './systems/renderer.js';
 import{Resizer} from './systems/Resizer.js';
 
+let camera;
+let renderer;
+let scene;
 class World{
-    constructor(container){}
-    render(){}
+    constructor(container){
+        camera = createCamera();
+        scene = createScene();
+        renderer = createRenderer();
+        container.append(renderer.domElement);
+    }
+    render(){
+        renderer.render(scene, camera);
+    }
 }
 export{World};
