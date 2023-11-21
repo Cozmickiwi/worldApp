1dafef4fec12884878872e896c791546845bf903
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-21 22:28:28 +1300

    import modules

--- src/World/World.js
+++ src/World/World.js
@@ -1,3 +1,11 @@
+// Import components
+import{createCamera} from './components/camera.js';
+import{createCube} from './components/cube.js';
+import{createScene} from './components/scene.js';
+// Import systems
+import{createRenderer} from './systems/renderer.js';
+import{Resizer} from './systems/Resizer.js';
+
 class World{
     constructor(container){}
     render(){}
