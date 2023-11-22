0e25ecc5d3e7bcf037ff433f9e3d85d1696cfb6a
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-22 18:20:41 +1300

    add settings function and variables

--- src/World/systems/animation.js
+++ src/World/systems/animation.js
@@ -5,27 +5,71 @@ let cube;
 let cube2;
 let cameraMod;
 let listCont = false;
+let cameraRev = false;
+let cameraZStatus = 10;
+let shape1XRot;
+let shape1YRot;
+let shape1ZRot;
+let shape2ZRot;
+let cameraZMax;
+let cameraZMin;
+let cameraZMov;
+let cameraZRevMov;
+let cameraZRot;
 const clock = new Clock();
 
-function objectNames(scene, camera){
+function rad(num){
+    return((Math.PI)/(360/num));
+}
+function settings(){
+    (function shape1Settings(){
+        shape1XRot = rad(1.15);
+        shape1YRot = rad(0.86);
+        shape1ZRot = rad(0.6); 
+    })();
+    (function shape2Settings(){
+        shape2ZRot = rad(0.4);
+    })();
+    (function cameraAnimSettings(){
+        cameraZMax = 25;
+        cameraZMin = 7;
+        cameraZMov = 0.15;
+        cameraZRevMov = 0.065;
+        cameraZRot = rad(0.6);
+    })();
+}
+function init(scene, camera){
     cube = scene.getObjectByName('cube1');
     cube2 = scene.getObjectByName('cube2');
     cameraMod = camera;
+    settings();
 }
 function shape1(timeScale){
-    cube.rotation.x += (0.02 * timeScale);
-    cube.rotation.y += (0.015 * timeScale);
-    cube.rotation.z += (0.01 * timeScale);
+    cube.rotation.x += (shape1XRot * timeScale);
+    cube.rotation.y += (shape1YRot * timeScale);
+    cube.rotation.z += (shape1ZRot * timeScale);
 }
 function shape2(timeScale){
-    cube2.rotation.z += (0.007 * timeScale);
+    cube2.rotation.z += (shape2ZRot * timeScale);
 }
 function cameraAnim(timeScale){
-    cameraMod.rotation.z += (0.01 * timeScale);
+    cameraMod.rotation.z += (cameraZRot * timeScale);
+    if(cameraRev == false && (cameraZStatus)<cameraZMax){
+        cameraMod.position.z += (cameraZMov * timeScale);
+        cameraZStatus += (cameraZMov * timeScale);
+    }
+    else{
+        cameraRev = true;
+        cameraMod.position.z -= (cameraZRevMov * timeScale);
+        cameraZStatus -= (cameraZRevMov * timeScale);
+        if((cameraZStatus)<cameraZMin){
+            cameraRev = false;
+        }
+    }
 }
 export function animateMod(scene, camera){
     const delta = clock.getDelta();
-    objectNames(scene, camera);
+    init(scene, camera);
     if(listCont == false){
         list.push(shape1);
         list.push(shape2);
