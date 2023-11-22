17a8915e530daffcc3cf556cba8297f09da85754
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-22 16:16:26 +1300

    add antialiasing

--- src/World/systems/renderer.js
+++ src/World/systems/renderer.js
@@ -1,7 +1,9 @@
 import { WebGLRenderer } from '../../../node_modules/three/build/three.module.js';
 
 function createRenderer(){
-    const renderer = new WebGLRenderer();
+    const renderer = new WebGLRenderer({
+        antialias: true,
+    });
     renderer.useLegacyLights = true;
     return renderer;
 }
