7379776a08b2a901f2837ad99aa7ad0b624902dc
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-22 19:21:05 +1300

    lower light intensity

--- src/World/components/lights.js
+++ src/World/components/lights.js
@@ -1,7 +1,7 @@
 import { DirectionalLight } from '../../../node_modules/three/build/three.module.js';
 
 function createLights(){
-    const light = new DirectionalLight('white', 8);
+    const light = new DirectionalLight('white', 2);
     light.position.set(10, 10, 10);
     return light;
 }
