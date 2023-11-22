30d033e26ca0b64d5d68bf64faed9bca3092200f
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-23 03:57:28 +1300

    add orbitcontrols

--- src/World/systems/controls.js
+++ src/World/systems/controls.js
@@ -1,5 +1,11 @@
-import{OrbitControls} from '../../../node_modules/three/examples/jsm/controls/OrbitControls';
+import{OrbitControls} from '../../../node_modules/three/examples/jsm/controls/OrbitControls.js';
 
-function createControls(){}
+function createControls(camera, canvas){
+    const controls = new OrbitControls(camera, canvas);
+    controls.enableDamping = true;
+    controls.dampingFactor= 0.075;
+    //controls.listenToKeyEvents(window);
+    return controls;
+}
 
 export{createControls};
