f9d5c01ce13591f549956153d831772af732b1d9
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-23 03:54:20 +1300

    add floor and controls

--- src/World/World.js
+++ src/World/World.js
@@ -12,25 +12,28 @@ import{createControls} from './systems/controls.js';
 let camera;
 let renderer;
 let scene;
+let controls;
 class World{
     constructor(container){
         camera = createCamera();
         scene = createScene();
         renderer = createRenderer();
         container.append(renderer.domElement);
+        //controls = createControls(camera, renderer.domElement);
         const cube = createCube(true);
         const cube2 = createCube(false);
+        const floor = createCube('floor');
         cube.name = 'cube1';
         cube2.name = 'cube2';
-        cube.translateZ(-80);
+        //cube.translateZ(-80);
         const light = createLights();
-        scene.add(cube, cube2, light);
+        scene.add(cube, cube2, floor, light);
         const resizer = new Resizer(container, camera, renderer);
     }
     render(){
         function animate(){
             window.requestAnimationFrame(animate);
-            animateMod(scene, camera);
+            animateMod(scene, camera, controls);
             renderer.render(scene, camera);
         }
         renderer.render(scene, camera);
