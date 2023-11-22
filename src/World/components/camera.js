84dd386c47b52efa7797bf634a4a83dd5a4deeea
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-22 18:20:09 +1300

    change camera far value

--- src/World/components/camera.js
+++ src/World/components/camera.js
@@ -1,7 +1,7 @@
 import { PerspectiveCamera } from '../../../node_modules/three/build/three.module.js';
 
 function createCamera(){
-    const camera = new PerspectiveCamera(35, 1, 0.1, 100,);
+    const camera = new PerspectiveCamera(35, 1, 0.1, 300);
     camera.position.set(0, 0, 10);
     camera.rotation.set(0, .0, 1.5);
     return camera;
