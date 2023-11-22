326c402f8093c127b0bf161747446f4e1b993170
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-23 03:56:14 +1300

    add flat plane

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -1,15 +1,18 @@
 import {
     IcosahedronGeometry, 
     TorusGeometry, 
+    PlaneGeometry,
     Mesh, 
     MeshStandardMaterial, 
     MeshToonMaterial,
     TextureLoader,
+    DoubleSide,
 } from '../../../node_modules/three/build/three.module.js';
 
 function createCube(first){
-    const geometry = new IcosahedronGeometry(4.5);
+    const geometry = new IcosahedronGeometry(2.5);
     const geometry2 = new TorusGeometry(6.5, .7);
+    const geometry3 = new PlaneGeometry(600, 600, 100, 100);
     const textureLoader = new TextureLoader();
     const texture = textureLoader.load(
         '/src/World/components/assets/space-cruiser-panels2-unity/space-cruiser-panels2_albedo.png',
@@ -30,18 +33,32 @@ function createCube(first){
         wireframeLinecap: 'round',
         roughness: .8,
     });
+    const material3 = new MeshStandardMaterial({
+        color: 'CornflowerBlue',
+        wireframe: true,
+        wireframeLinecap: 'roundSkyBlueDarkTurquoise',
+        roughness: 0.1,
+    })
     const toon = new MeshToonMaterial({
         color: 'blue',
+        
     })
     const cube = new Mesh(geometry, material);
     const cube2 = new Mesh(geometry2, material2);
-    cube2.position.set(-.5, 0, -20);
-    cube2.rotation.y = 2.4;
+    const plane = new Mesh(geometry3, material3);
+    //cube2.position.set(0, 0, -20);
+    //cube2.rotation.y = 2.4;
+    plane.rotation.x = ((Math.PI*2)/(360/90));
+    plane.position.set(0, -10, 0)
+    plane.material.side = DoubleSide;
     if(first == true){
         return (cube);
     }
-    else{
+    else if(first == false){
         return (cube2);
+    }
+    else{
+        return (plane)
     }
 }
 export{createCube};
