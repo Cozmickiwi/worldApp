769fb3f356c7557586d03efc0119ad350a5f9bca
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-22 22:56:23 +1300

    add wad doom style controls

--- src/World/systems/animation.js
+++ src/World/systems/animation.js
@@ -16,11 +16,49 @@ let cameraZMin;
 let cameraZMov;
 let cameraZRevMov;
 let cameraZRot;
+let cameraXRot;
+let keyPressed = false;
+let controlupd;
+let cameraPosX = false;
+let cameraPosXRev = false;
+let cameraFoward = false;
+let currentCamPos = 0;
+let rightSensitivity;
+let leftSensitivity;
+let fowardSpeed;
+let backSpeed;
 const clock = new Clock();
 
 function rad(num){
     return((Math.PI)/(360/num));
 }
+
+window.addEventListener('keydown', (event) => {
+    if(event.key == 'a'){
+        cameraPosXRev = true;
+        cameraPosX = false;
+    }
+    else if(event.key == 'd'){
+        cameraPosX = true;
+        cameraPosXRev = false;
+    }
+    else if(event.key == 'w'){
+        cameraFoward = true;
+    }
+})
+window.addEventListener('keyup', (event) => {
+    if(event.key == 'a'){
+        cameraPosXRev = false;
+    }
+    else if(event.key == 'd'){
+        cameraPosX = false;
+    }
+    else if(event.key == 'w'){
+        cameraFoward = false;
+    }
+})
+
+
 function settings(){
     (function shape1Settings(){
         shape1XRot = rad(1.15);
@@ -36,6 +74,12 @@ function settings(){
         cameraZMov = 0.15;
         cameraZRevMov = 0.065;
         cameraZRot = rad(0.6);
+    })();
+    (function controlSettings(){
+        rightSensitivity = 1;
+        leftSensitivity = 1;
+        fowardSpeed = 0.6;
+        backSpeed = 0.2;
     })();
 }
 function init(scene, camera){
@@ -53,6 +97,7 @@ function shape2(timeScale){
     cube2.rotation.z += (shape2ZRot * timeScale);
 }
 function cameraAnim(timeScale){
+    /*
     cameraMod.rotation.z += (cameraZRot * timeScale);
     if(cameraRev == false && (cameraZStatus)<cameraZMax){
         cameraMod.position.z += (cameraZMov * timeScale);
@@ -66,10 +111,67 @@ function cameraAnim(timeScale){
             cameraRev = false;
         }
     }
+    */
+    if(cameraPosX == true){
+        cameraMod.rotation.y -= (rad(rightSensitivity) * timeScale);
+        currentCamPos -= (rightSensitivity * timeScale)/2;
+        if(currentCamPos<=-360){
+            currentCamPos += 360;
+        }
+    }
+    if(cameraPosXRev == true){
+        cameraMod.rotation.y += (rad(leftSensitivity) * timeScale);
+        currentCamPos += (leftSensitivity * timeScale)/2;
+        if(currentCamPos>=360){
+            currentCamPos -= 360;
+        }
+    }
+    if(cameraFoward == true){
+        console.log(currentCamPos)
+        if(currentCamPos == 0){
+            cameraMod.position.z -= (0.2 * timeScale);
+        }
+        else if(currentCamPos<0 && currentCamPos>-90){
+            let direction = (90+currentCamPos)*.01;
+            cameraMod.position.z -= ((fowardSpeed*direction) * timeScale);
+            cameraMod.position.x += ((fowardSpeed-(fowardSpeed*direction)) * timeScale);
+            //console.log((0.2*direction) * timeScale);
+            //console.log((0.2-(0.2*direction)) * timeScale);
+        }
+        else if(currentCamPos>0 && currentCamPos<90){
+            let direction = (90-currentCamPos)*.01;
+            cameraMod.position.z -= ((fowardSpeed*direction) * timeScale);
+            cameraMod.position.x -= ((fowardSpeed-(fowardSpeed*direction)) * timeScale);
+            //console.log((0.2*direction) * timeScale);
+            //console.log((0.2-(0.2*direction)) * timeScale);
+        }
+        else if(currentCamPos>90 && currentCamPos<180){
+            let direction = (90-currentCamPos)*.01;
+            cameraMod.position.z -= ((fowardSpeed*direction) * timeScale);
+            cameraMod.position.x -= ((fowardSpeed+(fowardSpeed*direction)) * timeScale);
+            //console.log((0.2*direction) * timeScale);
+            //console.log((0.2+(0.2*direction)) * timeScale);
+        }
+        else if(currentCamPos<-90 && currentCamPos>-180){
+            let direction = (90+currentCamPos)*.01;
+            cameraMod.position.z -= ((fowardSpeed*direction) * timeScale);
+            cameraMod.position.x += ((fowardSpeed+(fowardSpeed*direction)) * timeScale);
+            //console.log((0.2*direction) * timeScale);
+            //console.log((0.2+(0.2*direction)) * timeScale);
+        }
+        if(currentCamPos <= -180){
+            currentCamPos = (180-((currentCamPos*(-1))-180));
+        }
+        else if(currentCamPos >= 180){
+            currentCamPos = (-1*(180+(180-currentCamPos)));
+        }
+    }
 }
-export function animateMod(scene, camera){
+export function animateMod(scene, camera, ){
     const delta = clock.getDelta();
     init(scene, camera);
+    //controls.update();
+    //controlupd = controls;
     if(listCont == false){
         list.push(shape1);
         list.push(shape2);
