e330d9e459030e1e4ade57bb712fb1a69f304494
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-23 21:54:44 +1300

    import pointer controls

--- src/World/systems/controls.js
+++ src/World/systems/controls.js
@@ -1,7 +1,7 @@
-import{OrbitControls} from '../../../node_modules/three/examples/jsm/controls/OrbitControls.js';
+import{PointerLockControls} from '../../../node_modules/three/examples/jsm/controls/PointerLockControls.js';
 
 function createControls(camera, canvas){
-    const controls = new OrbitControls(camera, canvas);
+    const controls = new PointerLockControls(camera, canvas);
     controls.enableDamping = true;
     controls.dampingFactor= 0.075;
     //controls.listenToKeyEvents(window);
