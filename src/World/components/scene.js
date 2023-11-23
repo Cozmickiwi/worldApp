e8240f337822c570af9b3bcdbefa0a47ac9fd685
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-24 04:43:31 +1300

    comment

--- src/World/components/scene.js
+++ src/World/components/scene.js
@@ -1,10 +1,11 @@
-import { Color, Scene } from '../../../node_modules/three/build/three.module.js';
+import { Color, Scene, Fog } from '../../../node_modules/three/build/three.module.js';
 
 function createScene(ortho){
     const scene = new Scene();
     scene.background = new Color('skyblue');
     const sceneOrtho = new Scene();
     //sceneOrtho.background = new Color('blue');
+    //scene.fog = new Fog( 0xcccccc, 600, 750 );
     if(ortho == false){
         return scene;
     }
