90bd417e3ac962df92bf0b29be0678ec1429996c
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-24 04:39:17 +1300

    add wall and floor textures

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -7,6 +7,7 @@ import {
     MeshToonMaterial,
     TextureLoader,
     DoubleSide,
+    RepeatWrapping,
 } from '../../../node_modules/three/build/three.module.js';
 
 
@@ -15,10 +16,23 @@ function createCube(first){
     const geometry = new IcosahedronGeometry(2.5);
     const geometry2 = new TorusGeometry(6.5, .7);
     const geometry3 = new PlaneGeometry(600, 600, 100, 100);
+    const geometry4 = new PlaneGeometry(600, 100);
     const textureLoader = new TextureLoader();
     const texture = textureLoader.load(
         '/src/World/components/assets/space-cruiser-panels2-unity/space-cruiser-panels2_albedo.png',
     );
+    const wallTexture = textureLoader.load(
+        '/src/World/components/assets/white-grey-tile-wall.avif',
+    )
+    const floorTexture = textureLoader.load(
+        '/src/World/components/assets/pngtree-brick.jpg'
+    )
+    wallTexture.wrapS = RepeatWrapping;
+    wallTexture.wrapT = RepeatWrapping;
+    wallTexture.repeat.set(12,2)
+    floorTexture.wrapS = RepeatWrapping;
+    floorTexture.wrapT = RepeatWrapping;
+    floorTexture.repeat.set(20,20)
     const material = new MeshStandardMaterial({
         //color: 0x0000ff2f,
         wireframe: false,
@@ -36,11 +50,17 @@ function createCube(first){
         roughness: .8,
     });
     const material3 = new MeshStandardMaterial({
-        color: 'CornflowerBlue',
-        wireframe: true,
-        wireframeLinecap: 'roundSkyBlueDarkTurquoise',
-        roughness: 0.1,
-    })
+        color: 'grey',
+        //wireframe: true,
+        //roughness: 1,
+        map: floorTexture,
+    });
+    const material4 = new MeshStandardMaterial({
+        color: 'LightSteelBlue',
+        //wireframe: true,
+        //roughness: 0.1,
+        map: wallTexture,
+    });
     const toon = new MeshToonMaterial({
         color: 'blue',
         
@@ -48,20 +68,25 @@ function createCube(first){
     const cube = new Mesh(geometry, material);
     const cube2 = new Mesh(geometry2, material2);
     const plane = new Mesh(geometry3, material3);
+    const wall1 = new Mesh(geometry4, material4);
     cube.position.set(0,10,0);
     cube2.position.set(0, 10, 0);
     //cube2.rotation.y = 2.4;
     plane.rotation.x = ((Math.PI*2)/(360/90));
     //plane.position.set(0, -10, 0)
     plane.material.side = DoubleSide;
+    wall1.material.side = DoubleSide;
     if(first == true){
         return (cube);
     }
     else if(first == false){
         return (cube2);
     }
-    else{
+    else if(first == 'floor'){
         return (plane)
+    }
+    else if(first == 'wall1'){
+        return(wall1)
     }
 }
 export{createCube};
