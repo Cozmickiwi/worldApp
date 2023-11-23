5f033f8710df5e130011b09f2470eb8cc5203ef8
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-24 04:41:54 +1300

    add stats plugin

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import {World} from "./World/World.js";
-
+import Stats from '../node_modules/three/examples/jsm/libs/stats.module.js';
 function main(){
     const container = document.getElementById('scene-container');
     const world = new World(container);
@@ -10,3 +10,20 @@ button.addEventListener('click', () => {
     button.style.display = 'none';
     main();
 });
+var stats = new Stats();
+stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild( stats.dom );
+
+function animate() {
+
+	stats.begin();
+
+	// monitored code goes here
+
+	stats.end();
+
+	requestAnimationFrame( animate );
+
+}
+
+requestAnimationFrame( animate );
