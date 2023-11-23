a2c6ed669cafc81e0f6ff6a5b7108f1f515af4f0
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-24 04:42:13 +1300

    add gun animations

--- src/World/World.js
+++ src/World/World.js
@@ -18,6 +18,11 @@ import{
     Mesh,
     MeshBasicMaterial,
     TextureLoader,
+    AnimationMixer,
+    Clock,
+    LoopOnce,
+    LoopRepeat,
+    Matrix4,
 } from '../../node_modules/three/build/three.module.js';
 
 const gltfLoader = new GLTFLoader();
@@ -28,6 +33,17 @@ let controls;
 let ortho;
 let orthoScene;
 let locked = false;
+let gunModelLoaded = false;
+let mixer;
+let action;
+let shootAction;
+let walkAction;
+let runAction;
+let walking = false;
+let clock = new Clock();
+let shoot = false;
+let movementStatus;
+//let canvas = document.querySelector('canvas');
 let gunMaterial = new MeshBasicMaterial({
 
 })
@@ -41,9 +57,6 @@ class World{
         ortho = createOrtho();
         container.append(renderer.domElement);
         controls = createControls(camera, renderer.domElement);
-        let xP = document.getElementById('x');
-        let yP = document.getElementById('y');
-        let zP = document.getElementById('z');
         let doomButton = document.getElementById('doomControl');
         //controls.current.connect();
         //controls.current.lock();
@@ -57,34 +70,42 @@ class World{
                 locked = false;
             }
         })
-        window.addEventListener('mousemove', () => {
-            //console.log(get)
-            xP.textContent = (`x:${camera.position._x}`);
-            yP.textContent = (`y:${camera.position._y}`);
-            zP.textContent = (`z:${camera.position._z}`);
-        })
-        window.addEventListener('keydown', () => {
-            //controls.moveForward(1);
-        })
         const cube = createCube(true);
         const cube2 = createCube(false);
         const floor = createCube('floor');
+        const wall1 = createCube('wall1');
+        const wall2 = createCube('wall1');
+        const wall3 = createCube('wall1');
+        const wall4 = createCube('wall1');
         cube.name = 'cube1';
         cube2.name = 'cube2';
         //cube.translateZ(-80);
         const light = createLights(false);
         const light2 = createLights(true);
-        scene.add(cube, cube2, floor, light);
+        const light3 = createLights('amb');
+        function walls(){
+            wall1.position.set(0,0,-300);
+            wall2.position.set(0,0,300);
+            wall3.position.set(300,0,0);
+            wall3.rotation.set(0,(Math.PI)/(360/180),0);
+            wall4.position.set(-300,0,0);
+            wall4.rotation.set(0,-(Math.PI)/(360/180),0);
+        }
+        walls()
+        scene.add(cube, cube2, floor, wall1, wall2, wall3, wall4, light, light3);
         gltfLoader.load(
-            '/src/World/components/assets/ultrakillgun/silahful2.glb',
+            //'/src/World/components/assets/ultrakillgun/silahful2.glb',
+            '/src/World/components/assets/gunAnim/source/untitled.glb',
             function(gltf){
                 //gltf.scene.name = 'gun';
                 let gun1 = gltf.scene;
-                
+                //console.log(gun1)
                 //let gun2 = new Mesh (gun1, MeshBasicMaterial);
                 //orthoScene.add(gun2);
-                gun1.scale.set(12,12,38);
-                gun1.position.set(350,700,1)
+                //gun1.scale.set(12,12,38);
+                //gun1.position.set(350,700,1)
+                gun1.scale.set(-2000,2000,2000);
+                gun1.position.set(150,3020,1)
                 gun1.name = 'gun';
                 const textureLoader = new TextureLoader();
                 const texture = textureLoader.load(
@@ -96,20 +117,83 @@ class World{
                 
                 //gun1.rotation.z = (1)
                 //gun1.rotation.X = (1.5)
-                gun1.rotation.set(-Math.PI/2,-.1,0)
+                gun1.rotation.set(-Math.PI/2,-.1,-0.15)
                 gun1.rotation.x = (3.1)
-                
+                //gun1.applyMatrix()
                 //renderer.render(orthoScene, ortho);
+                
+                console.log(gltf.animations)
+                mixer = new AnimationMixer(gltf.scene);
+                const clips = gltf.animations;
+                let animationNum = 3;
+                action = mixer.clipAction(gltf.animations[3]);
+                shootAction = mixer.clipAction(gltf.animations[1]);
+                walkAction = mixer.clipAction(gltf.animations[10]);
+                //action.play();
+                
+                document.addEventListener('mousedown', (event) => {
+                        if(event.target != document.getElementById('doomControl')){
+                            shoot = true;
+                        }
+                        
+                        //shootAction.setLoop(LoopRepeat);
+                    
+                })
+                document.addEventListener('keydown', (event) => {
+                    if(event.key == 'Control'){
+                        shoot = true;
+                    }
+                })
+                gunModelLoaded = true;
+                /*
+                function update () {
+                    mixer.update( deltaSeconds );
+                }
+                clips.forEach( function ( clip ) {
+                    mixer.clipAction( clip ).play();
+                } );
+                */
                 orthoScene.add(gltf.scene, light2);
+
             }
         )
-        
         const resizer = new Resizer(container, camera, renderer);
     }
     render(){
         function animate(){
             window.requestAnimationFrame(animate);
-            animateMod(scene, camera, controls);
+            if(gunModelLoaded == true){
+                //walkAction.setLoop(LoopRepeat);
+                if(shoot == true){
+                    shootAction.play();
+                    shootAction.setLoop(LoopOnce);
+                    shootAction.reset();
+                    shoot = false;
+                }
+                else if(movementStatus == 'walk'){
+                    walkAction.play();
+                    walkAction.setLoop(LoopRepeat);
+                    //walkAction.reset();
+                }
+                else if(walking == true && movementStatus == 'still'){
+                    
+                    walkAction.play();
+                    walkAction.setLoop(LoopOnce);
+                    walkAction.stop();
+                    walking = false;
+                }
+                else{
+                    walkAction.play();
+                    walkAction.setLoop(LoopOnce);
+                    walkAction.stop();
+                    walking = false;
+                    action.play();
+                }
+                
+                mixer.update(clock.getDelta());
+                //action = baseAction;
+            }
+            movementStatus = animateMod(scene, camera, controls);
             renderer.clear();
             renderer.render(scene, camera);
             renderer.clearDepth();
