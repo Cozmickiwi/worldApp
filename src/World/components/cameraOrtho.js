3494130c189821964804d2512ea28d61cf236746
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-24 04:42:56 +1300

    adjust ortho cam values

--- src/World/components/cameraOrtho.js
+++ src/World/components/cameraOrtho.js
@@ -1,6 +1,6 @@
 import { OrthographicCamera } from '../../../node_modules/three/build/three.module.js';
 function createOrtho(){
-    const orthoCamera = new OrthographicCamera(-innerWidth/2, innerWidth/2, -innerHeight/2, innerHeight/2, -10000, 1000);
+    const orthoCamera = new OrthographicCamera(-innerWidth/2, innerWidth/2, -innerHeight/2, innerHeight/2, 230, 1000);
     orthoCamera.position.set(1,1,10)
     return orthoCamera;
 }
