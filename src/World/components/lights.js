514af9f05e8496563f5a43578a59688c4f5807ab
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-24 04:43:17 +1300

    adjust lighting and add ambient light

--- src/World/components/lights.js
+++ src/World/components/lights.js
@@ -8,13 +8,18 @@ import {
 function createLights(amb){
     const light = new DirectionalLight('white', 2);
     light.position.set(20, 20, 10);
-    const light2 = new DirectionalLight('aliceblue', .5);
-    light2.position.set(100,-100,-500)
+    const light2 = new DirectionalLight('aliceblue', 3);
+    //light2.position.set(100,-100,-500)
+    light2.position.set(20, 20, 10);
+    const light3 = new AmbientLight('white', .3);
     if(amb == false){
         return light;
     }
-    else{
+    else if(amb == true){
         return light2;
+    }
+    else if(amb == 'amb'){
+        return light3;
     }
 }
 export{createLights};
