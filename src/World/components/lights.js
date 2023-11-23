04baca5cc84f0a10f70ea44eb919af64793fbb06
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-23 21:52:38 +1300

    add new light for gun

--- src/World/components/lights.js
+++ src/World/components/lights.js
@@ -1,8 +1,20 @@
-import { DirectionalLight } from '../../../node_modules/three/build/three.module.js';
+import { 
+    DirectionalLight,
+    AmbientLight,
+    SpotLight,
+    PointLight
+} from '../../../node_modules/three/build/three.module.js';
 
-function createLights(){
+function createLights(amb){
     const light = new DirectionalLight('white', 2);
     light.position.set(20, 20, 10);
-    return light;
+    const light2 = new DirectionalLight('aliceblue', .5);
+    light2.position.set(100,-100,-500)
+    if(amb == false){
+        return light;
+    }
+    else{
+        return light2;
+    }
 }
 export{createLights};
