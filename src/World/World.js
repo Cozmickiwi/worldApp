3075b726233a28fa50291ef74193a452347e47d5
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-25 04:51:09 +1300

    add character to game

--- src/World/World.js
+++ src/World/World.js
@@ -36,20 +36,31 @@ let orthoScene;
 let locked = false;
 let gunModelLoaded = false;
 let mixer;
+let mixer2;
+let personAction;
 let action;
 let shootAction;
 let walkAction;
+let vector = new Vector3;
 let runAction;
 let fireImg;
 let walking = false;
 let clock = new Clock();
+let clock2 = new Clock();
 let shoot = false;
 let movementStatus;
 let pos;
+let person;
 let boxPos;
 let cube;
+let playerBox;
+let playerBB;
+let boxBB;
 let box;
+let enemyCollision = false;
 let shotSound = new Audio('/src/World/components/assets/gunsound2.mp3')
+let personPos;
+let personDmg = 0;
 //let canvas = document.querySelector('canvas');
 let gunMaterial = new MeshBasicMaterial({
 
@@ -86,11 +97,15 @@ class World{
         const wall4 = createCube('wall1');
         const crossHair = createCube('cross');
         const fire = createCube('fire');
+        playerBox = createCube('playerBox');
         box = createCube('box');
+        playerBB = createCube('pbb');
+        boxBB = createCube('bbb')
         fire.name = 'fireFlash';
-        
         cube.name = 'cube1';
         cube2.name = 'cube2';
+        playerBox.name = 'playerBox';
+        console.log(playerBB);
         //cube.translateZ(-80);
         const light = createLights(false);
         const light2 = createLights(true);
@@ -104,7 +119,8 @@ class World{
             wall4.rotation.set(0,-(Math.PI)/(360/180),0);
         }
         walls()
-        scene.add(cube, cube2, floor, wall1, wall2, wall3, wall4, box, light, light3);
+        scene.add(cube, cube2, floor, wall1, wall2, wall3, wall4, box, playerBox, light, light3);
+        //playerBB = scene.getObjectByName('playerBB')
         gltfLoader.load(
             //'/src/World/components/assets/ultrakillgun/silahful2.glb',
             '/src/World/components/assets/gunAnim/source/untitled.glb',
@@ -173,6 +189,30 @@ class World{
                 
             }
         )
+        gltfLoader.load(
+            //'/src/World/components/assets/ultrakillgun/silahful2.glb',
+            '/src/World/components/assets/gunAnim/source/untitled2222.glb',
+            function(gltf){
+                
+                person = gltf.scene;
+                person.scale.set(7,7,7)
+                person.position.set(-50,0,-50)
+                mixer2 = new AnimationMixer(gltf.scene);
+                personAction = mixer2.clipAction(gltf.animations[0]);
+                console.log(gltf.animations)
+                personAction.timeScale = 1.25;
+                scene.add(person);
+                setInterval(() => {
+                    if((person.position.x < (camera.position.x-15) || person.position.x > (camera.position.x+15))
+                    ||(person.position.z < (camera.position.z-15)|| person.position.z > (camera.position.z+15))){
+                        enemyCollision = false;
+                    }
+                    else{
+                        enemyCollision = true;
+                    }
+                }, 200);
+            }
+        )
         const resizer = new Resizer(container, camera, renderer);
     }
     render(){
@@ -183,8 +223,8 @@ class World{
                 if(shoot == true){
                     pos = new Vector3( 0, 0, -(Math.sqrt((camera.position.z**2)+(camera.position.x**2))) ).applyQuaternion( camera.quaternion ).add( camera.position );
                     boxPos = new Vector3( 0, 0, -(Math.sqrt(((camera.position.z-100)**2)+((camera.position.x-100)**2))) ).applyQuaternion( camera.quaternion ).add( camera.position );
-                    console.log(scene);
-                    console.log(pos);
+                    //console.log(scene);
+                    console.log(boxPos)
                     if((pos.z<(2) && pos.z>(-2))&&(pos.x<(2) && pos.x>(-2))&&(pos.y<12.5&&pos.y>7.5)){
                         cube.visible = false;
                         setTimeout(() => {
@@ -197,19 +237,39 @@ class World{
                             box.visible = true;
                         }, 1000);
                     }
+                    if((personPos.z<(person.position.z+2) && personPos.z>(person.position.z-2))&&(personPos.x<(person.position.x+2) && personPos.x>(person.position.x-2)) && (personPos.y>0 && personPos.y<=20)){
+                        //person.visible = false;
+                        //console.log(personPos);
+                        if(personPos.y > 10.3 && personPos.y < 11.8 && personPos.z<(person.position.z+1) && personPos.z>(person.position.z-1)&&(personPos.x<(person.position.x+1) && personPos.x>(person.position.x-1))){
+                            personDmg += 3;
+                        }
+                        else{
+                            personDmg +=1;
+                        }
+                        if(personDmg >= 3){
+                            personDmg = 0;
+                            person.position.set((Math.floor(Math.random()*600))-300, 0, (Math.floor(Math.random()*600))-300);
+                        }
+                        
+                    }
                     //console.log(camera.getWorldDirection( ))
-                    shotSound.load();
+                    console.log(playerBB);
                     shootAction.play();
+                    shotSound.load();
                     shotSound.play();
+                    console.log(playerBox)
+                    console.log(camera.getWorldDirection(vector))
                     fireImg.visible = true;
                     setTimeout(() => {
                         fireImg.visible = false;
                     }, 50);
-                    console.log(camera)
+                    //console.log(camera)
                     shootAction.setLoop(LoopOnce);
                     shootAction.reset();
                     shoot = false;
                     fireImg.rotation.z = (Math.random()*(Math.PI));
+                    console.log(person.position);
+                    
                 }
                 else if(movementStatus == 'walk'){
                     walkAction.play();
@@ -230,11 +290,26 @@ class World{
                     walking = false;
                     action.play();
                 }
-                
-                mixer.update(clock.getDelta());
+                personAction.play();
+                person.lookAt(camera.position.x, camera.position.y, camera.position.z);
+                personPos = new Vector3( 0, 0, -(Math.sqrt(((camera.position.z-(person.position.z))**2)+((camera.position.x-(person.position.x))**2))) ).applyQuaternion( camera.quaternion ).add( camera.position );
+                //if(person.position.x > camera.position.x+15 || person.position.x < camera.position.x-15 || person.position.z > camera.position.z+15 || person.position.z < camera.position.z-15){
+                //    person.translateZ(.3);
+                //    person.position.y = 0.1;
+                //}
+                if(enemyCollision == false){
+                    person.translateZ(.3);
+                    person.position.y = 0.1;
+                }
+                //console.log(person.position);
                 //action = baseAction;
             }
-            movementStatus = animateMod(scene, camera, controls);
+            movementStatus = animateMod(scene, camera, controls, playerBB, boxBB);
+            if(gunModelLoaded == true){
+                mixer.update(clock.getDelta());
+                mixer2.update(clock2.getDelta());
+            }
+            
             renderer.clear();
             renderer.render(scene, camera);
             renderer.clearDepth();
