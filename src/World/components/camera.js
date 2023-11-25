af96ff04bd10bdefecbaf57dce4e13baca2dc8b4
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-26 02:34:38 +1300

    camera rotation for testing

--- src/World/components/camera.js
+++ src/World/components/camera.js
@@ -4,7 +4,7 @@ function createCamera(){
     const camera = new PerspectiveCamera(35, 1, 0.1, 1000);
     camera.position.set(0, 10, 50);
     //camera.maxPolar
-    //camera.rotation.set(0, .0, 0);
+    //camera.rotation.y = (Math.PI);
     return camera;
 }
 export{createCamera};
