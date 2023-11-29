b3be6819e03742c97c2e08aaf4ebeadff36d3fb2
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-29 21:45:21 +1300

    height adjustments

--- src/World/components/camera.js
+++ src/World/components/camera.js
@@ -2,7 +2,7 @@ import { PerspectiveCamera } from '../../../node_modules/three/build/three.modul
 
 function createCamera(){
     const camera = new PerspectiveCamera(55, 1, 0.1, 1000);
-    camera.position.set(0, 10, 50);
+    camera.position.set(50, 10, 0);
     //camera.maxPolar
     //camera.rotation.y = (Math.PI);
     return camera;
