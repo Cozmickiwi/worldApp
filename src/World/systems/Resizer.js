32a0ba180cc196f13b14a4101a9f535c1d184b5b
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-12-01 00:44:52 +1300

    make canvas pixelated

--- src/World/systems/Resizer.js
+++ src/World/systems/Resizer.js
@@ -2,7 +2,7 @@ const setSize = (container, camera, renderer) => {
     camera.aspect = container.clientWidth / container.clientHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(container.clientWidth, container.clientHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setPixelRatio(window.devicePixelRatio/4);
 }
 
 class Resizer{
