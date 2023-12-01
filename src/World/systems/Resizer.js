adfc8cdb035d3deab431613caa707a99604c92dd
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-12-01 21:08:43 +1300

    adjust pixel ratio

--- src/World/systems/Resizer.js
+++ src/World/systems/Resizer.js
@@ -2,7 +2,7 @@ const setSize = (container, camera, renderer) => {
     camera.aspect = container.clientWidth / container.clientHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(container.clientWidth, container.clientHeight);
-    renderer.setPixelRatio(window.devicePixelRatio/4);
+    renderer.setPixelRatio(window.devicePixelRatio/2);
 }
 
 class Resizer{
