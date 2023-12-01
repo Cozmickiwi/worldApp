ed9a639be8d2d48ddd88a958d7ab55ea6d1b3031
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-12-01 20:05:28 +1300

    change camera fov and height

--- src/World/components/camera.js
+++ src/World/components/camera.js
@@ -1,8 +1,8 @@
 import { PerspectiveCamera } from '../../../node_modules/three/build/three.module.js';
 
 function createCamera(){
-    const camera = new PerspectiveCamera(55, 1, 0.1, 1000);
-    camera.position.set(50, 10, 0);
+    const camera = new PerspectiveCamera(45, 1, 0.1, 1000);
+    camera.position.set(50, 5, 0);
     //camera.maxPolar
     //camera.rotation.y = (Math.PI);
     return camera;
