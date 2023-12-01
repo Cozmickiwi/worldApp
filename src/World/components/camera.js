212f07ec203cf10850c122b874ec62ef94e69baa
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-12-01 21:08:32 +1300

    change starting position

--- src/World/components/camera.js
+++ src/World/components/camera.js
@@ -2,9 +2,9 @@ import { PerspectiveCamera } from '../../../node_modules/three/build/three.modul
 
 function createCamera(){
     const camera = new PerspectiveCamera(45, 1, 0.1, 1000);
-    camera.position.set(50, 5, 0);
+    camera.position.set(-20, 5, 240);
     //camera.maxPolar
-    //camera.rotation.y = (Math.PI);
+    camera.rotation.y = (-Math.PI/2);
     return camera;
 }
 export{createCamera};
