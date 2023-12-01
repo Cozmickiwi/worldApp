50621c221404fd0d15c814d5cb4aea92280c1c5e
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-12-01 20:06:28 +1300

    perfomance tweaks

--- src/World/systems/renderer.js
+++ src/World/systems/renderer.js
@@ -3,9 +3,12 @@ import { WebGLRenderer } from '../../../node_modules/three/build/three.module.js
 function createRenderer(){
     const renderer = new WebGLRenderer({
         //antialias: true,
+        powerPreference: 'high-performance',
+        stencil: false,
     });
     renderer.useLegacyLights = true;
     renderer.autoClear = false;
+
     return renderer;
 }
 export{createRenderer};
